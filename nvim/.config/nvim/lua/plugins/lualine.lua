07c422a7b33faf163728d14caeae851ae024bd70
balamurugan-testpress <balamurugan@testpress.in>
2025-11-29 18:18:18 +0530

    remove the tmuxline from the config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,107 +1,89 @@
 return {
-  {
-    "edkolev/tmuxline.vim",
-    event = "VeryLazy",
-  },
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  enabled = true,
+  lazy = true,
+  opts = function()
+    local tokyonight = require("lualine.themes.tokyonight")
 
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-
-    opts = function()
-      -- Load and safely patch TokyoNight theme
-      local theme = require("lualine.themes.tokyonight")
-
-      -- Transparent backgrounds, but still valid for tmuxline
-      for _, mode in pairs(theme) do
-        for _, hl in pairs(mode) do
-          if hl.bg then
-            hl.bg = "" -- EMPTY STRING = valid (NONE = INVALID in tmuxline)
-          end
-        end
+    -- Make background transparent
+    for _, section in pairs(tokyonight) do
+      for _, hl in pairs(section) do
+        hl.bg = "NONE"
       end
+    end
 
-      return {
-        options = {
-          theme = theme,
-          globalstatus = true,
-          icons_enabled = true,
-          section_separators = { left = "", right = "" },
-          component_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = { "alpha", "dashboard", "starter", "neo-tree" },
+    return {
+      options = {
+        theme = tokyonight,
+        globalstatus = true,
+        icons_enabled = true,
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = { "alpha", "dashboard", "starter", "neo-tree" },
+        },
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = {
+          { "branch", icon = "" },
+          {
+            "diff",
+            symbols = { added = " ", modified = " ", removed = " " },
           },
         },
-
-        sections = {
-          lualine_a = {},
-          lualine_b = {
-            { "branch", icon = "" },
-            {
-              "diff",
-              symbols = { added = " ", modified = " ", removed = " " },
+        lualine_c = {
+          {
+            "filename",
+            path = 0,
+            symbols = {
+              modified = " ●",
+              readonly = " 🔒",
+              unnamed = "😒",
+              newfile = " ",
             },
           },
-          lualine_c = {
-            {
-              "filename",
-              path = 0,
-              symbols = {
-                modified = " ●",
-                readonly = " 🔒",
-                unnamed = "😒",
-                newfile = " ",
-              },
-            },
-          },
-          lualine_x = {
-            {
-              "diagnostics",
-              symbols = {
-                error = " ",
-                warn = " ",
-                info = " ",
-                hint = " ",
-              },
-              always_visible = true,
-            },
-            { "filetype", icon_only = true },
-            { "fileformat" },
-          },
-          lualine_y = { "location" },
-          lualine_z = {},
         },
-
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {
-            {
-              "filename",
-              path = 0,
-              symbols = {
-                modified = " ●",
-                readonly = " 🔒",
-                unnamed = "😒",
-              },
+        lualine_x = {
+          {
+            "diagnostics",
+            symbols = {
+              error = " ",
+              warn = " ",
+              info = " ",
+              hint = " ",
+            },
+            always_visible = true,
+          },
+          { "filetype", icon_only = true },
+          { "fileformat" },
+        },
+        lualine_y = {
+          {
+            "location",
+          },
+        },
+        lualine_z = {},
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          {
+            "filename",
+            path = 0,
+            symbols = {
+              modified = " ●",
+              readonly = " 🔒",
+              unnamed = "😒",
             },
           },
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = {},
         },
-      }
-    end,
-
-    config = function(_, opts)
-      require("lualine").setup(opts)
-
-      -- Run :Tmuxline before :TmuxlineSnapshot
-      vim.defer_fn(function()
-        vim.cmd("Tmuxline tokyonight")                      -- ① generate tmuxline theme
-        vim.cmd("TmuxlineSnapshot! ~/.tmux/tmuxline.conf")  -- ② write snapshot
-      end, 300)
-    end,
-  },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      },
+    }
+  end,
 }
-
